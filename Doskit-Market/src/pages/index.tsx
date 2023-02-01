4be05843ef7b96cf82573969cf212aa97d8883a2
Kazeem Dosunmu <97337763+thedoskit@users.noreply.github.com>
2023-02-01 18:31:23 +0100

    hotfix

--- Doskit-Market/src/pages/index.tsx
+++ Doskit-Market/src/pages/index.tsx
@@ -1,46 +1,40 @@
 import type { NextPage } from "next";
-import {
-  useActiveListings,
-  useAddress,
-  useContract,
-} from "@thirdweb-dev/react";
+import { ConnectWallet, useActiveListings, useAddress, useContract } from "@thirdweb-dev/react";
 import Link from "next/link";
 import NFTCard from "../components/NFTCard";
 
 const Home: NextPage = () => {
   const address = useAddress();
-
-  const { contract } = useContract(
-    '0x675295C86E2e52Af2Efd74dA610ac8E9713a3944',
-    'marketplace'
-  );
-
+  const { contract } = useContract('0x675295C86E2e52Af2Efd74dA610ac8E9713a3944', 'marketplace');
   const { data: nfts, isLoading } = useActiveListings(contract);
 
   if (isLoading)
     return (
-      <div className={"mb-3 flex w-screen justify-center"}>Loading ...</div>
+      <div className="mb-3 flex w-screen justify-center">Loading ...</div>
     );
 
   return (
-    <div className={"space-y-4 p-2"}>
-      <div className={"flex space-x-4"}>
-        <div className={"text-2xl font-semibold"}>Doskit</div>
-        <Link href={`profile/${address}`}>
-          <div className={"cursor-pointer text-2xl font-semibold"}>
-            Marketplace
-          </div>
-        </Link>
-      </div>
-      <div className={`nft-grid`}>
+<div className="p-2">
+  <nav className="flex items-center justify-between px-6 py-3 bg-gray-800">
+    <div className="flex items-center">
+      <img src="" alt="Logo" />
+      <h1 className="text-2xl font-semibold text-white ml-2">Doskit Marketplace</h1>
+    </div>
+    <div className="flex">
+      <ConnectWallet />
+    </div>
+  </nav>
+  <main className="min-h-screen">
+    <div className={`nft-grid`}>
+      <div className="flex flex-wrap">
         {nfts &&
-          nfts.map((nft) => {
+          nfts.map((nft, index) => {
             return (
               <Link
                 href={`/assets/${nft.id}`}
                 key={nft.assetContractAddress + nft.id}
               >
-                
+                <div className="w-3/3">
                   <NFTCard
                     nft={{
                       name: nft.asset.name as string,
@@ -48,12 +42,18 @@ const Home: NextPage = () => {
                       price: nft.buyoutCurrencyValuePerToken?.displayValue,
                     }}
                   />
-                
+                </div>
               </Link>
             );
           })}
       </div>
     </div>
+  </main>
+  <footer className="h-16 bg-gray-800 text-white text-center">
+    <p>Copyright © Doskit Marketplace 2023. Built by Kazeem Dosunmu.</p>
+  </footer>
+</div>
+
   );
 };
 
